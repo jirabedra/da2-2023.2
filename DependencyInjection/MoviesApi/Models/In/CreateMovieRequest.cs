e6e09fd931829f6b226ecd8ebc468fe01dfb5262
jirabedra <juaniirabedra@gmail.com>
2023-08-03 18:14:06 -0300

    added DI example base

---
+++ DependencyInjection/MoviesApi/Models/In/CreateMovieRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesApi.Models.In
+{
+    public class CreateMovieRequest
+    {
+        public string Title { get; set; }
+        public IEnumerable<string> Genres { get; set; }
+    }
+}
